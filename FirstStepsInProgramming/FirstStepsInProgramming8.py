72b6fd9a8b4d3d917ee9a315f83442e70e72d6ed
AsenSirakov <asko2040@abv.bg>
2024-02-28 20:37:26 +0200

    Solution Upload
    FirstStepsInProgramming

---
+++ FirstStepsInProgramming/FirstStepsInProgramming8.py
@@ -0,0 +1,6 @@
+value1 = int(input())
+dog = value1 * 2.50
+value2 = int(input())
+cat = value2 * 4
+total = dog + cat
+print(f"{total} lv.")
---
+++ FirstStepsInProgramming/FirstStepsInProgramming9.py
@@ -0,0 +1,6 @@
+field = float(input())
+price = field * 7.61
+offer = price * 0.18
+total = price - offer
+print(f"The final price is: {total} lv.")
+print(f'The discount is: {offer} lv.')
